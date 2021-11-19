c9125f6902baf0c1515ed26bed07a2ac7f3cd61d
Mike van der Boon <git@mvdboon.nl>
2021-11-19 18:14:14 +0100

    Personalize

--- {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__init__.py
+++ {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__init__.py
@@ -1,6 +1,18 @@
 # type: ignore[attr-defined]
-"""{{ cookiecutter.project_description }}"""
+"""{{ cookiecutter.project_description }}
 
+A one line summary of the module or program, terminated by a period.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
+
+  Typical usage example:
+
+  foo = ClassFoo()
+  bar = foo.FunctionBar()
+"""
 import sys
 
 if sys.version_info >= (3, 8):
@@ -10,6 +22,26 @@ else:
 
 
 def get_version() -> str:
+    """Fetches rows from a Smalltable.
+
+    Retrieves rows pertaining to the given keys from the Table instance
+    represented by table_handle.  String keys will be UTF-8 encoded.
+
+    Args:
+        table_handle: An open smalltable.Table instance.
+
+    Returns:
+        A dict mapping keys to the corresponding table row data
+        fetched. Each row is represented as a tuple of strings. For
+        example:
+
+        {b'Serak': ('Rigel VII', 'Preparer'),
+         b'Zim': ('Irk', 'Invader'),
+         b'Lrrr': ('Omicron Persei 8', 'Emperor')}
+
+    Raises:
+        IOError: An error occurred accessing the smalltable.
+    """
     try:
         return importlib_metadata.version(__name__)
     except importlib_metadata.PackageNotFoundError:  # pragma: no cover
--- {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
+++ {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
@@ -12,6 +12,15 @@ from {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}
 
 
 class Color(str, Enum):
+    """Summary of class here.
+
+    Longer class information....
+    Longer class information....
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
     white = "white"
     red = "red"
     cyan = "cyan"
